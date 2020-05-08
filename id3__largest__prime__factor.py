a2b87a0e90323e84535845ea8628657c2bd63b87
KRISHNA KUMAR <60271340+krsign@users.noreply.github.com>
2020-05-08 11:19:17 +0530

    Add files via upload

--- id3__largest__prime__factor.py
+++ id3__largest__prime__factor.py
@@ -6,4 +6,13 @@
     
     @krsign
 """
+def primeFactor(n):
+    i = 2
+    while i * i < n:
+        while n % i == 0:
+            n = n / i
+        i = i + 1
+    return n
 
+print(primeFactor(600851475143))
+        
